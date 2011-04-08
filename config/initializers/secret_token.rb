b4f99cf0e127e4bcb48ce43baf18291c59d8aefd
Robert Sheldon <robert.sheldon@tufts.edu>
2011-04-07 22:31:02 -0400

    added all files ever

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalProject::Application.config.secret_token = '19062ded956df0436c58134cec468d4e291652f989329a0816d4d611fd0ffd566e2f60526da8d6dfab28e21da81fd6ac840d13eb5efbe03d4796cf242fae9806'
